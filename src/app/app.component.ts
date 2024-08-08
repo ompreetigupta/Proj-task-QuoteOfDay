5028a98e9f2101047eced4f4c6e40c6a5a1bb464
ompreetigupta <110689124+ompreetigupta@users.noreply.github.com>
2024-08-08 21:39:03 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,31 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Proj-Api';
+  http=inject(HttpClient);
+
+  /**
+   *
+   */
+  constructor() {
+
+    this.http.get('https://localhost:7256/get_tasks',
+      {
+        observe: 'response',
+      }).subscribe((r)=>{
+        console.log(r.headers.get('Content-Type'));
+
+        const customHeader =r.headers.get('X-Custom-Header');
+
+        const body = r.body;
+        console.log(body);
+
+      })
+
+
+
+    
+    
+    
+  }
+
 }
